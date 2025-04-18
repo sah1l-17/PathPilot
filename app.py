51d03d1b1ffd04218010f014ab4280e45589c64f
sah1l-17 <123564297+sah1l-17@users.noreply.github.com>
2025-04-19 02:26:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def index():
     return render_template("index.html", advice=career_advice, interests=interests, skills=skills)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
